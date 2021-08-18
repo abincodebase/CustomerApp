8bac7c3e29dafa64c3290d4ff73b3a9ab3d59cc9
abincodebase <60213367+abincodebase@users.noreply.github.com>
2021-08-18 09:26:18 +0530

    Delete CustomerApp directory

--- CustomerApp/src/main/java/com/customerapp/jpa/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.customerapp.jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.customerapp.jpa.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
